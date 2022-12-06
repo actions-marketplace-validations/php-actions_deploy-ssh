726bcae6c9e5cf4e6069c5299eee885eb07b59a4
Greg Bowler <greg.bowler@g105b.com>
2022-12-06 18:08:20 +0000

    wip: remove debug line

--- deploy-action.bash
+++ deploy-action.bash
@@ -34,21 +34,5 @@ tar -czf - --exclude-vcs . | \
 	-i "$ssh_key_path" \
 	-p "$ACTION_PORT" \
 	"$ACTION_USER"@"$ACTION_HOSTNAME" \
-	"rm -rf $full_transfer_path && mkdir -p $full_transfer_path && cd $full_transfer_path && tar -xzf - && mv ./post-transfer.bash $ACTION_POST_TRANSFER_SCRIPT && echo $ACTION_POST_TRANSFER_SCRIPT_PREFIX $ACTION_POST_TRANSFER_SCRIPT && cat $ACTION_POST_TRANSFER_SCRIPT && $ACTION_POST_TRANSFER_SCRIPT_PREFIX $ACTION_POST_TRANSFER_SCRIPT"
+	"rm -rf $full_transfer_path && mkdir -p $full_transfer_path && cd $full_transfer_path && tar -xzf - && mv ./post-transfer.bash $ACTION_POST_TRANSFER_SCRIPT && echo $ACTION_POST_TRANSFER_SCRIPT_PREFIX $ACTION_POST_TRANSFER_SCRIPT && $ACTION_POST_TRANSFER_SCRIPT_PREFIX $ACTION_POST_TRANSFER_SCRIPT"
 echo "Transfer complete"
-
-#action_dir="$(dirname -- "${BASH_SOURCE[0]}")"
-#{ declare -p \
-#	ACTION_PATH \
-#	GITHUB_REF_NAME \
-#	full_transfer_path \
-#	; \
-#cat "$action_dir"/remote-script.bash; } | \
-#	ssh \
-#	-i "$ssh_key_path" \
-#	-p "$ACTION_PORT" \
-#	"$ACTION_USER"@"$ACTION_HOSTNAME" \
-#	bash -s
-
-# TODO:
-# remote-script.bash
