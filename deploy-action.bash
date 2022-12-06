0e459cf279f35968f189c21ec830a37da26fee50
Greg Bowler <greg.bowler@g105b.com>
2022-12-06 11:15:15 +0000

    wip: sudo call post-transfer script

--- deploy-action.bash
+++ deploy-action.bash
@@ -1,34 +1,50 @@
 #!/bin/bash
 set -e
-full_transfer_path="$ACTION_TRANSFER_PATH/$GITHUB_SHA"
+# Remove trailing slashes from path variables:
+ACTION_PATH=${ACTION_PATH%/}
+ACTION_TRANSFER_PATH=${ACTION_TRANSFER_PATH%/}
+
+# Initiate SSH environment for remote connection:
 mkdir -p ~/.ssh
 ssh-keyscan -t rsa "$ACTION_HOSTNAME" >> ~/.ssh/known_hosts
 ssh_key_path=~/.ssh/action_rsa
 echo "$ACTION_SSH_KEY" > "$ssh_key_path"
 chmod g-rw,o-rw "$ssh_key_path"
-cd "$GITHUB_WORKSPACE"
-dir_size_human=$(du -sbh --exclude "./.git" | grep -o "[0-9]*")
-echo "Transferring $dir_size_human bytes to $ACTION_HOSTNAME..."
-tar -czf - --exclude-vcs . | \
-	ssh \
-	-i "$ssh_key_path" \
-	-p "$ACTION_PORT" \
-	"$ACTION_USER"@"$ACTION_HOSTNAME" \
-	"mkdir -p $full_transfer_path && cd $full_transfer_path && tar -xzf -"
-echo "Transfer complete"
 
+cd "$GITHUB_WORKSPACE"
+# Pass in required variables to post-transfer script:
 action_dir="$(dirname -- "${BASH_SOURCE[0]}")"
 { declare -p \
 	ACTION_PATH \
 	GITHUB_REF_NAME \
 	full_transfer_path \
 	; \
-cat "$action_dir"/remote-script.bash; } | \
+cat "$action_dir"/remote-script.bash; } > ./post-transfer.bash
+chmod +x ./post-transfer.bash
+# Archive directory and pipe over SSH:
+dir_size_human=$(du -sbh --exclude "./.git" | grep -o "[0-9]*")
+echo "Transferring $dir_size_human bytes to $ACTION_HOSTNAME..."
+full_transfer_path="$ACTION_TRANSFER_PATH/$GITHUB_SHA"
+tar -czf - --exclude-vcs . | \
 	ssh \
 	-i "$ssh_key_path" \
 	-p "$ACTION_PORT" \
 	"$ACTION_USER"@"$ACTION_HOSTNAME" \
-	bash -s
+	"rm -rf $full_transfer_path && mkdir -p $full_transfer_path && cd $full_transfer_path && tar -xzf - && sudo ./post-transfer.bash"
+echo "Transfer complete"
+
+#action_dir="$(dirname -- "${BASH_SOURCE[0]}")"
+#{ declare -p \
+#	ACTION_PATH \
+#	GITHUB_REF_NAME \
+#	full_transfer_path \
+#	; \
+#cat "$action_dir"/remote-script.bash; } | \
+#	ssh \
+#	-i "$ssh_key_path" \
+#	-p "$ACTION_PORT" \
+#	"$ACTION_USER"@"$ACTION_HOSTNAME" \
+#	bash -s
 
 # TODO:
 # remote-script.bash
---
+++ remote-post-transfer.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+full_transfer_path=${full_transfer_path:-"/dev/null"}
+source_dir="$full_transfer_path"
+destination_dir="$ACTION_PATH/$GITHUB_REF_NAME"
+destination_dir_prev="$destination_dir.previous"
+
+echo "Started post-transfer script with source ($source_dir) destination ($destination_dir)"
+
+if [[ -d "$destination_dir" ]]
+then
+	if [[ -d "$destination_dir_prev" ]]
+	then
+		echo rm -rf "$destination_dir_prev"
+	fi
+
+	echo mv "$destination_dir" "$destination_dir_prev"
+fi
+
+echo mv "$source_dir" "$destination_dir"
+
+echo "Completed post-transfer script"
+
+# TODO:
+# Add symlinks from a definition list somewhere
+# Set a special deployment config variable
